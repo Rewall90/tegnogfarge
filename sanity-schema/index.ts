5a813f91ec2d360fb7fc719144b55466fffce3f4
Petter Lund <petterlund@hotmail.no>
2025-06-06 11:01:17 +0200

    Fix Sanity schema imports for better compatibility

--- sanity-schema/index.ts
+++ sanity-schema/index.ts
@@ -1,1 +1,11 @@
-export { schemaTypes } from '../sanity-studio/schemaTypes'; 
+import category from '../sanity-studio/schemaTypes/category';
+import drawingImage from '../sanity-studio/schemaTypes/drawingImage';
+import post from '../sanity-studio/schemaTypes/post';
+import subcategory from '../sanity-studio/schemaTypes/subcategory';
+
+export const schemaTypes = [
+  category,
+  subcategory,
+  drawingImage,
+  post
+]; 
