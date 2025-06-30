a214c8b2a2e3c48a1c54939ddede548d5ac1a752
Petter Lund <petterlund@hotmail.no>
2025-06-30 12:35:10 +0200

    fix(config): remove conflicting redirects from next.config.js

--- next.config.js
+++ next.config.js
@@ -19,22 +19,6 @@ const nextConfig = {
     deviceSizes: [320, 420, 520, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  async redirects() {
-    return [
-      // Redirect www to non-www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.tegnogfarge.no',
-          },
-        ],
-        destination: 'https://tegnogfarge.no/:path*',
-        permanent: true,
-      },
-    ]
-  },
   async headers() {
     return [
       {
